bc454243e22f22cb3b0d239dfb05af117c5b65a6
Mike Di Domenico <mike@mouseware.net>
2020-10-22 17:37:36 -0400

    v1.0.1 fix for missing attribute in HTTP get

--- src/button-add.js
+++ src/button-add.js
@@ -7,30 +7,54 @@ window.addEventListener("load", function (e) {
 		var sourceVideoObj = document.getElementsByTagName("video")[0];
 		var sourceVideoUrl = sourceVideoObj.getAttribute("src");
 
-		// Generate the download button
-		var recordingDownloadForm = document.createElement("form");
-		recordingDownloadForm.setAttribute("method", "get");
-		recordingDownloadForm.setAttribute("action", sourceVideoUrl);
+		var matches = sourceVideoUrl.match(/(.*)\?(.*)/);
 
-		var recordingDownloadButton = document.createElement("button");
-		recordingDownloadButton.setAttribute("id", "recording-download-button");
-		recordingDownloadButton.setAttribute("type", "submit");
-		recordingDownloadButton.innerHTML = "Download";
-		recordingDownloadButton.style.top = "0px";
-		recordingDownloadButton.style.right = "0px";
-		recordingDownloadButton.style.position = "fixed";
-		recordingDownloadButton.style.padding = "20px";
-		recordingDownloadButton.style.backgroundColor = "#ffffff";
-		recordingDownloadButton.style.zIndex = "100";
-		recordingDownloadButton.style.border = "0px";
-		recordingDownloadButton.style.borderBottomLeftRadius = "5px";
-		recordingDownloadButton.style.borderBottom = "1px solid #cccccc";
-		recordingDownloadButton.style.borderLeft = "1px solid #cccccc";
+		if (matches.length > 1) {
+			var url = matches[1];
+			var params = matches[2].split("&");
 
-		recordingDownloadForm.innerHTML = recordingDownloadButton.outerHTML;
+			// Generate the download button
+			var recordingDownloadForm = document.createElement("form");
+			recordingDownloadForm.setAttribute("method", "get");
+			recordingDownloadForm.setAttribute("action", url);
 
-		// Insert it in the page
-		var appDiv = document.getElementById("app");
-		appDiv.insertAdjacentHTML("afterbegin", recordingDownloadForm.outerHTML);
+			// Add all of the hidden fields
+			if (params.length > 0) {
+				for (var i = 0; i < params.length; i++) {
+					var setMatch = params[i].match(/(.*)=(.*)/);
+
+					if (setMatch.length > 1) {
+						var hiddenInputField = document.createElement("input");
+						hiddenInputField.setAttribute("type", "hidden");
+						hiddenInputField.setAttribute("name", setMatch[1]);
+						hiddenInputField.value = setMatch[2];
+
+						recordingDownloadForm.innerHTML = recordingDownloadForm.innerHTML + hiddenInputField.outerHTML;
+					}
+				}
+			}
+
+			// Add the download button
+			var recordingDownloadButton = document.createElement("button");
+			recordingDownloadButton.setAttribute("id", "recording-download-button");
+			recordingDownloadButton.setAttribute("type", "submit");
+			recordingDownloadButton.innerHTML = "Download";
+			recordingDownloadButton.style.top = "0px";
+			recordingDownloadButton.style.right = "0px";
+			recordingDownloadButton.style.position = "fixed";
+			recordingDownloadButton.style.padding = "20px";
+			recordingDownloadButton.style.backgroundColor = "#ffffff";
+			recordingDownloadButton.style.zIndex = "100";
+			recordingDownloadButton.style.border = "0px";
+			recordingDownloadButton.style.borderBottomLeftRadius = "5px";
+			recordingDownloadButton.style.borderBottom = "1px solid #cccccc";
+			recordingDownloadButton.style.borderLeft = "1px solid #cccccc";
+
+			recordingDownloadForm.innerHTML = recordingDownloadForm.innerHTML + recordingDownloadButton.outerHTML;
+
+			// Insert it in the page
+			var appDiv = document.getElementById("app");
+			appDiv.insertAdjacentHTML("afterbegin", recordingDownloadForm.outerHTML);
+		}
 	}
 });
